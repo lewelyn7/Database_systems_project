3cb2f5dfa87558f38b0c2809b7256d5b25beae0c
lewelyn7 <karol.hamielec@gmail.com>
2020-05-23 19:44:16 +0200

    pushhhh

--- wstawiacz_roboczy.py
+++ wstawiacz_roboczy.py
@@ -1,4 +1,5 @@
 from neo4j import GraphDatabase
+from random import randint
 import csv
 
 # class HelloWorldExample(object):
@@ -24,20 +25,35 @@ import csv
 
 driver1 = GraphDatabase.driver("bolt://bazy.flemingo.ovh:7687", auth=("neo4j", "marcjansiwikania"))
 
-def create_subjects(tx):
-    infile = open("przedmioty.csv", "r")
+def create_subjects(tx, filename, faculty_name):
+    infile = open(filename, "r")
     csvimport = csv.reader(infile)
     for row in csvimport:
         print(row[0])
-        tx.run("CREATE (a:Subject) SET a.name = $name", name=row[0])
+        result = tx.run("MATCH (n:Subject { name : $name }) RETURN n", name=row[0])
+        if not result.single():
+            tx.run("CREATE (a:Subject) SET a.name = $name", name=row[0])
+            tx.run("MATCH (a:Subject { name : $name }),(b:Faculty { name : $faculty }) CREATE (a)-[r:BelongsTo]->(b)", name=row[0], faculty=faculty_name)
+        else:
+            tx.run("MATCH (a:Subject { name : $name }),(b:Faculty { name : $faculty }) CREATE (a)-[r:BelongsTo]->(b)", name=row[0], faculty=faculty_name)
 
-def create_tutors(tx):
-    infile = open("wykladowcy.csv", "r")
+def create_tutors(tx, faculty_name):
+    infile = open("wykladowcy.csv", "r")  
     csvimport = csv.reader(infile)
     for row in csvimport:
         print(row[0])
         tx.run("CREATE (a:Tutor) SET a.firstname = $firstname, a.lastname = $lastname, a.mail = $mail, a.degree = $degree", firstname=row[1], lastname=row[0], mail=row[3], degree=row[2])
-
+        tx.run("MATCH (a:Tutor { firstname: $firstname, lastname: $lastname}), (b:Faculty { name : $faculty }) CREATE (a)-[r:WorksIn]->(b)")
+        subjectnum = tx.run("MATCH (a:Faculty { name = $faculty}) WITH count(a) AS value RETURN value", faculty=faculty_name).single()[0]
+        
+        numberofsubjects = randint(1,4)
+        for i in range(numberofsubjects):
+            randomnum = randint(0, subjectnum)
+            subjects = tx.run("MATCH (a:Subject { faculty : $faculty})", faculty=faculty_name)
+            subject = subjects[randomnum]
+        
 
 with driver1.session() as session:
-    session.write_transaction(create_tutors)
+    session.write_transaction(create_subjects, "przedmioty.csv", "Informatyki")
+    session.write_transaction(create_subjects, "przedmioty2.csv", "Elektroniki")
+    session.write_transaction(create_subjects, "przedmioty3.csv", "Fizyki Medycznej")
