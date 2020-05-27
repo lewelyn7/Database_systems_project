5c0396f3fe58146e5abf759806e769244529e362
lewelyn7 <karol.hamielec@gmail.com>
2020-05-27 23:53:19 +0200

    data_insert attends added

--- src/data_insert.py
+++ src/data_insert.py
@@ -35,27 +35,6 @@ def create_subjects(tx, filename, faculty_name):
         tx.run("Match (a:Subject), (b:Subject) where a.name = $name and a.tier = 6 and id(b) = $id Create (a)-[r:Require]->(b)", name = row[0], id = basics_5[randint(0, len(basics_5)-1)]['id'])
         tx.run("Match (a:Subject), (b:Subject) where a.name = $name and a.tier = 7 and id(b) = $id Create (a)-[r:Require]->(b)", name = row[0], id = basics_6[randint(0, len(basics_6)-1)]['id'])
 
-def create_subjects2(tx, filename, faculty_name):
-    infile = open(filename, "r")
-    csvimport = csv.reader(infile)    
-    for row in csvimport:
-        print(row[0])
-        result = tx.run("MATCH (n:Subject { name : $name }) RETURN n", name=row[0])
-        if not result.single():
-            rand_tier = randint(1, 7)
-            tx.run("CREATE (a:Subject) SET a.name = $name, a.tier = $tier", name=row[0], tier = rand_tier)
-            tx.run("MATCH (a:Subject { name : $name }),(b:Faculty { name : $faculty }) CREATE (a)-[r:BelongsTo]->(b)", name=row[0], faculty=faculty_name)
-        else:
-            tx.run("MATCH (a:Subject { name : $name }),(b:Faculty { name : $faculty }) CREATE (a)-[r:BelongsTo]->(b)", name=row[0], faculty=faculty_name)
-
-    tiers = []
-    for i in range(1,8):
-        tiers.append(tx.run("MATCH (s:Subject { tier : $tier}) RETURN s.name as id", tier=i).data())      
-
-    for item in tiers.reverse():
-        quantity = randint(1,3)
-        for i in range(quantity):
-            pass
 
 def create_tutors(tx, lecturers_file, faculty_name):
     infile = open(lecturers_file, "r")  
@@ -108,6 +87,22 @@ def sign_students(tx, filename):
                     print(courses[randomcourse])
                     tx.run("MATCH (s:Student {pesel: $pesel}), (b:Subject) WHERE b.name=$subid CREATE (s)-[r:Completed]->(b)", pesel=row[2], subid=courses[randomcourse])
 
+#not documented
+def set_attends_rel(tx, filename):
+    infile = open(filename, "r")
+    csvimport = csv.reader(infile)
+
+    for row in csvimport:
+            courses_ava = tx.run("match (s:Student {pesel: $pesel})-[:Completed]->(:Subject)<-[:Require]-(sub:Subject) return distinct sub.name as id", pesel=row[2]).data()
+            courses1 = [item['id'] for item in courses_ava]
+            completed_courses_dict = tx.run("match (s:Student {pesel: $pesel})-[:Completed]->(sub:Subject) return sub.name as id", pesel=row[2]).data()
+            completed_courses = [item['id'] for item in completed_courses_dict]
+            courses = list(set(courses1) - set(completed_courses))
+            how_many = randint(0, len(courses))
+            print(row[2] + " " + str(how_many))
+            print(courses)
+            for i in range(how_many):
+                tx.run("MATCH (s:Student {pesel: $pesel}), (sb:Subject {name: $name}) CREATE (s)-[:Attends]->(sb)", pesel=row[2], name=courses[i])
 
         
 if __name__ == '__main__':
@@ -119,6 +114,6 @@ if __name__ == '__main__':
         # session.write_transaction(create_tutors, "wykladowcy.csv", "Informatyki")
         # session.write_transaction(create_tutors, "wykladowcy2.csv", "Elektroniki")
         # session.write_transaction(create_tutors, "wykladowcy3.csv", "Fizyki Medycznej")
-        session.write_transaction(add_students, "students.csv")
+        # session.write_transaction(add_students, "students.csv")
         # session.write_transaction(sign_students, "students.csv")
-        sign_students(session, "students.csv")
+        set_attends_rel(session, "data/students.csv")
