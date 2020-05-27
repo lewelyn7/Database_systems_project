70107f832779979da2611c0ff81bf7a0f8e22c17
lewelyn7 <karol.hamielec@gmail.com>
2020-05-28 01:28:35 +0200

    some tests

--- src/gui_test/Student_dialog.py
+++ src/gui_test/Student_dialog.py
@@ -7,12 +7,13 @@ class StudentDialog(QtWidgets.QDialog, Ui_DialogStudentInfo):
         super(StudentDialog, self).__init__()
         self.setupUi(self)
         self.db = kwargs['db']
+        self.buttonBox.accepted.connect(self.DialogStudentInfo.accept)
 
     def OkClicked(self):
         fname = self.lineEdit.text()
         lname = self.lineEdit_2.text()
-        pesel = self.lineEdit_3.text()
-        album = self.lineEdit_4.text()
+        pesel = self.lineEdit_4.text()
+        album = self.lineEdit_3.text()
 
         if not fname:
             fname = None
@@ -24,6 +25,10 @@ class StudentDialog(QtWidgets.QDialog, Ui_DialogStudentInfo):
             pesel = None
 
         with self.db.driver1.session() as session:
-            self.result = session.write_transaction(DBHelpers.get_student_info, firstname=fname, lastname=lname, pesel=pesel, student_nr=album)
+            self.student_info = session.write_transaction(DBHelpers.get_student_info, firstname=fname, lastname=lname, pesel=pesel, student_nr=album)
+            self.completed = session.write_transaction(DBHelpers.get_student_completed_courses, firstname=fname, lastname=lname, pesel=pesel, student_nr=album)
+            self.attends = session.write_transaction(DBHelpers.get_student_attends_courses, firstname=fname, lastname=lname, pesel=pesel, student_nr=album)
+
+        return
     def CancelClicked(self):
         pass
---
+++ src/gui_test/signed_up_UI.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'signed_up.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(400, 122)
+        self.gridLayout = QtWidgets.QGridLayout(Dialog)
+        self.gridLayout.setObjectName("gridLayout")
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setObjectName("label_2")
+        self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
+        self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setObjectName("buttonBox")
+        self.gridLayout.addWidget(self.buttonBox, 2, 0, 1, 1)
+        self.lineEdit = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit.setObjectName("lineEdit")
+        self.gridLayout.addWidget(self.lineEdit, 0, 1, 1, 1)
+        self.lineEdit_2 = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.gridLayout.addWidget(self.lineEdit_2, 1, 1, 1, 1)
+
+        self.retranslateUi(Dialog)
+        self.buttonBox.accepted.connect(Dialog.accept)
+        self.buttonBox.rejected.connect(Dialog.reject)
+        self.buttonBox.clicked['QAbstractButton*'].connect(Dialog.OkClicked)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.label.setText(_translate("Dialog", "Album number"))
+        self.label_2.setText(_translate("Dialog", "Subject name"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
