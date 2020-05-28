58bfa4a87544ee4b83fb6c755adc9d2c079b59f2
zbrze <58784685+zbrze@users.noreply.github.com>
2020-05-28 10:17:36 +0200

    Update db_helpers.py

--- src/db_helpers.py
+++ src/db_helpers.py
@@ -206,7 +206,7 @@ class DBHelpers:
         
     @staticmethod   
     def sign_up(tx, course_name, student_nr):
-        if(tx.run("MATCH (s:Student {student_nr : $number})-[r:Attends]->(n:Subject {name : $name}) RETURN count(r)", number = student_nr, name = course_name).single()[0] != 0 ):
+        if tx.run("MATCH (s:Student {student_nr : $number})-[r]->(n:Subject {name : $name}) RETURN count(r)", number = student_nr, name = course_name).single()[0] != 0:
             print("Student has already been signed up for this course")
             return False
         if(DBHelpers.missing_required_subjects(tx, course_name, student_nr) == 'ok'):
